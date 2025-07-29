4412e5289785ff4f862f2225d6c61f578a5f0896
RetardedSlowpoke <teorgin@gmail.com>
2025-07-29 16:22:49 +0300

    MainPageTests, POM updated

--- src/main/java/MainPagePOM.java
+++ src/main/java/MainPagePOM.java
@@ -1,5 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPagePOM {
     private WebDriver driver;
@@ -9,31 +15,44 @@ public class MainPagePOM {
     private By logoYandex = By.className("Header_LogoYandex__3TSOI"); //Логотип яндекса
     private By logoScooter = By.className("Header_LogoScooter__3lsAR"); //Логотип самокатов
 
-    public MainPagePOM (WebDriver driver){
+    public MainPagePOM(WebDriver driver) {
         this.driver = driver;
     }
-    public void orderButtonHeaderClick(){
+
+    public void orderButtonHeaderClick() {
         driver.findElement(orderButtonHeader).click();
     }
-    public void orderButtonBottomClick(){
+
+    public void orderButtonBottomClick() {
         driver.findElement(orderButtonBottom).click();
     }
-    public void statusButtonClick(){
+
+    public void statusButtonClick() {
         driver.findElement(statusButton).click();
     }
-    public void logoYandexClick(){
+
+    public void logoYandexClick() {
         driver.findElement(logoYandex).click();
     }
-    public void logoScooterClick(){
+
+    public void logoScooterClick() {
         driver.findElement(logoScooter).click();
     }
-    public void faqTabClick(int index){
-        driver.findElement(By.id("accordion__heading-"+ index)).click();
+
+    public void faqTabClick(int index) {
+        WebElement element = driver.findElement(By.id("accordion__heading-" + index));//Использован скрипт из-за неясного поведения /assets/scooter.png
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element); //При нескольких запусках одного и того же кода он мог как перекрыть кнопку, так и нет.
     }
-    public String faqTabHeaderGetText (int index){
-        return driver.findElement(By.id("accordion__heading-"+ index)).getText();
+
+    public String faqTabGetText(int index) {
+        new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-" + index)));
+        return driver.findElement(By.id("accordion__panel-" + index)).getText();
     }
-    public String faqTabGetText (int index){
-        return driver.findElement(By.id("accordion__panel-"+ index)).getText();
+
+    public void scrollToFaqTab(int index) {
+        WebElement element = driver.findElement(By.id("accordion__heading-" + index));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element); //Скролл вместо закрытия окна с куками выглядит надёжнее.
     }
 }
+
